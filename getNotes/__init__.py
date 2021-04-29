d205038cbe85820f574580c269912f89e2cea638
Acheong95 <75439001+Acheong95@users.noreply.github.com>
2021-04-29 13:33:58 +0100

    Add files via upload

---
+++ getNotes/__init__.py
@@ -0,0 +1,22 @@
+import os
+import json
+from bson.json_util import dumps
+import pymongo
+import logging
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python getposts trigger function.')
+    try:
+        url = os.getenviron['mongodb-connstring']
+        client = pymongo.MongoClient(url)
+        database = client['lab2db']
+        collection = database['notes']
+
+        result = collection.find({})
+        result = dumps(result)
+
+        return func.HttpResponse(result, mimetype="application/json", charset="utf8", status_code=200)
+    except:
+        return func.HttpResponse("bad request", status_code=400)
